b3fa20d1d2438e852c0257ff954acb39832b385b
Yuntroll95 <yuntrollpark@gamail.com>
2022-07-31 21:17:08 +0900

    Fix: Router 에러 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
 
 const Router = () => {
   return (
